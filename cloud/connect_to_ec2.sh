c4db8bf078ba47d45c5b6eb1ded6c0d5cc94ee83
EvgeniyKuzmin <evgeniy.a.kuzmin@gmail.com>
2021-08-17 13:26:29 +0300

    Added ssh-connect script to ec2

---
+++ cloud/connect_to_ec2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Connect to my EC2 instance with SSH
+USERNAME='ubuntu'
+PRIVATE_KEY='~/.ssh/ec2_t3-micro_2021-08-17.pem'
+DNS='ec2-52-48-122-233.eu-west-1.compute.amazonaws.com'
+# ID='i-098acb9039b1016de'
+
+ssh -i $PRIVATE_KEY $USERNAME@$DNS
