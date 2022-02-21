8daa290596beec92c3db0a6c8d835ea1c6c3ffc2
EvgeniyKuzmin <evgeniy.a.kuzmin@gmail.com>
2022-02-21 13:26:30 +0300

    APP: updated env-files for docker-compose

--- cloud/aws-dev_course/09-serverless/credential_files.tf
+++ cloud/aws-dev_course/09-serverless/credential_files.tf
@@ -1,24 +1,29 @@
 locals {
   credential_file     = ".env.development"
   credentials_db_file = ".env.db.development"
+  db_credentials = {
+    db_name     = local.app_name
+    db_user     = local.db_username
+    db_password = random_password.db_password.result
+  }
 }
 
 resource "local_file" "credentials_env" {
   filename = "${local.app_path}/${local.credential_file}"
   content = templatefile(
     "${path.module}/templates/${local.credential_file}",
-    {
-      db_host       = local.db_host
-      db_name       = local.app_name
-      db_user       = local.db_username
-      db_password   = random_password.db_password.result
-      flask_secret  = random_password.app_secret.result
-      aws_region    = var.region
-      bucket_name   = aws_s3_bucket.images.id
-      bucket_prefix = "${local.fs_public_prefix}/"
-      sqs_name      = aws_sqs_queue.uploads_notification.name
-      ssn_arn       = aws_sns_topic.uploads_notification.id
-    }
+    merge(
+      local.db_credentials,
+      {
+        db_host       = local.db_host
+        flask_secret  = random_password.app_secret.result
+        aws_region    = var.region
+        bucket_name   = aws_s3_bucket.images.id
+        bucket_prefix = "${local.fs_public_prefix}/"
+        sqs_name      = aws_sqs_queue.uploads_notification.name
+        sns_arn       = aws_sns_topic.uploads_notification.id
+      }
+    )
   )
 }
 
@@ -26,10 +31,6 @@ resource "local_file" "credentials_db_env" {
   filename = "${local.app_path}/${local.credentials_db_file}"
   content = templatefile(
     "${path.module}/templates/${local.credentials_db_file}",
-    {
-      db_name     = local.app_name
-      db_user     = local.db_username
-      db_password = random_password.db_password.result
-    }
+    local.db_credentials,
   )
 }
