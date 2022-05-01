90d1b9c5a2799695743c40e5cd70672e3daee7ba
EvgeniyKuzmin <evgeniy.a.kuzmin@gmail.com>
2022-05-01 15:41:33 +0300

    BASH: updated ubuntu_install.sh

--- bash/ubuntu_install.sh
+++ bash/ubuntu_install.sh
@@ -3,65 +3,73 @@
 EMAIL="evgeniy.a.kuzmin@gmail.com"
 NAME="EvgeniyKuzmin"
 
+###############################################################################
+
 ### Unpdate system's packages
-echo "---=== Updating ===---"
+echo "--->>> Updating APT"
 sudo apt update
 sudo apt full-upgrade -y
 sudo apt autoremove -y
+echo "--->>> APT updated"
 
+###############################################################################
 
-### Install usefull utilites
+echo "--->>> Install common packages"
 # make - describing relations of files
 # wget - downloading files by HTTP, HTTP, FTP
 # curl - working with URL requests (send, download, etc.)
 # xclip - command line interface for working with clipboard
 # tk-dev - cross-platform graph toolkit for Tcl and X11
-echo "---=== Install common packages ===---"
 sudo apt install -y \
 	make \
 	wget \
 	curl \
 	xclip \
 	tk-dev
+echo "--->>> Common packages installed"
 
+###############################################################################
 
-### Install software
-echo "---=== Install software via snap ===---"
+echo "--->>> Install software via snap"
 sudo snap install sublime-text --classic
 sudo snap install \
 	bitwarden \
 	postman \
 	beekeeper-studio \
-	clementine \
-	vlc \
-	libreoffice \
 	chromium \
 	telegram-desktop \
 	skype \
-	teams \
-	zoom-client
+	teams
+	# vlc \
+	# libreoffice \
+	# clementine \
+	# zoom-client \
+echo "--->>> Software via snap installed"
 
+###############################################################################
 
-### Install GIT
-echo "---=== Install GIT ===---"
+echo "--->>> Install GIT"
 sudo apt install -y git
 git config --global user.name $NAME
 git config --global user.email $EMAIL
-git config --global core.editor subl
+git config --global core.editor nano
 ## 1. Generating a new SSH key
-# $ ssh-keygen -t "github_${NAME}" -C $EMAIL
+# KEY_PATH="${HOME}/.ssh/github_${NAME}"
+# ssh-keygen -t ed25519 -f $KEY_PATH -C $EMAIL -N '' -q
+# chmod 400 $KEY_PATH
 ## 2. Adding the new SSH key to the ssh-agent
-# $ eval "$(ssh-agent -s)"
-# $ ssh-add "~/.ssh/github_${NAME}"
+# eval "$(ssh-agent -s)"
+# ssh-add $KEY_PATH
 ## 3. Adding a new SSH key to your GitHub account
 # GitHub.com > Settings > SSH and GPG keys > Add SSH key
 # "Title": empty, "Key": content of "~/.ssh/github_${NAME}.pub"
 ## 4. Test your SSH connection
-# $ ssh -T git@github.com
+# ssh -T git@github.com
+echo "--->>> GIT installed"
 
+###############################################################################
 
-### Install Python
-echo "---=== Install Python ===---"
+echo "--->>> Install PyEnv"
 sudo apt install -y \
 	python3-pip \
 	python3-dev \
@@ -70,10 +78,11 @@ sudo apt install -y \
 	libssl-dev \
 	libffi-dev \
 	unixodbc-dev
+echo "--->>> PyEnv installed"
 
+###############################################################################
 
-### Install PyEnv
-echo "---=== Install PyEnv ===---"
+echo "--->>> Install PyEnv"
 sudo apt install -y --no-install-recommends \
 	make \
 	build-essential \
@@ -93,10 +102,7 @@ sudo apt install -y --no-install-recommends \
 	libffi-dev \
 	liblzma-dev
 
-curl https://pyenv.run | bash
-git clone \
-	https://github.com/pyenv/pyenv-virtualenv.git \
-	$(pyenv root)/plugins/pyenv-virtualenv
+curl -L https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer | bash
 
 echo '' >> ~/.bashrc
 echo '' >> ~/.bashrc
@@ -107,17 +113,17 @@ echo 'eval "$(pyenv init -)"'  >> ~/.bashrc
 echo 'eval "$(pyenv virtualenv-init -)"'  >> ~/.bashrc
 
 exec $SHELL
+echo "--->>> PyEnv installed"
 
+###############################################################################
 
-### Docker install
-echo "---=== Install Docker ===---"
+echo "--->>> Install Docker"
 sudo apt install -y \
 	curl \
 	apt-transport-https \
 	ca-certificates \
 	gnupg-agent \
 	software-properties-common
-
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
 	| sudo apt-key add -
@@ -144,58 +150,37 @@ sudo curl -L \
 	-o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+echo "--->>> Docker installed"
 
+###############################################################################
 
-### Install Dropbox
-echo '---=== Install Dropbox ===---'
+echo "--->>> Install Dropbox"
 cd ~
-
 wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" \
 	| tar xzf -
 
 ~/.dropbox-dist/dropboxd
+echo "--->>> Dropbox installed"
 
+###############################################################################
 
-### Install MS ODBC
-sudo su
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/19.10/prod.list > /etc/apt/sources.list.d/mssql-release.list
-exit
-
-# sudo apt full-upgrade -y
-sudo apt update -y
-sudo ACCEPT_EULA=Y apt install \
-	msodbcsql17 \
-	mssql-tools \
-	unixodbc-dev
-
-
-echo '' >> ~/.bashrc
-echo '' >> ~/.bashrc
-echo '### MSSQL settings' >> ~/.bashrc
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-
-source ~/.bashrc
-
-
-# Install AWS
 echo "--->>> Install AWS"
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
 echo "--->>> AWS installed: \"$(aws --version)\""
 
+###############################################################################
 
-# Install AWS-Vault
 echo "--->>> Install AWS-Vault"
 sudo curl -L -o /usr/local/bin/aws-vault https://github.com/99designs/aws-vault/releases/download/v6.2.0/aws-vault-linux-amd64
 sudo chmod 755 /usr/local/bin/aws-vault
-echo "--->>> Severless installed: \"$(aws-vault --version)\""
+echo "--->>> AWS-Vault installed"
 # To add rcom apply the following and specify aws creds
 # aws-vault add rcom
 
+###############################################################################
 
-# Install NodeJS, NPM
 echo "--->>> Install NodeJS"
 sudo apt install nodejs
 echo "--->>> NodeJS installed: \"$(nodejs -v)\""
@@ -204,12 +189,13 @@ echo "--->>> Install NPM"
 sudo apt install npm
 echo "--->>> NPM installed: \"$(npm -v)\""
 
+###############################################################################
 
-# Install Serverless
 echo "--->>> Install Serverless"
 npm install -g serverless
 echo "--->>> Severless installed: \"$(serverless --version)\""
 
+###############################################################################
 
 echo "--->>> Install OPSWAT Client"
 wget https://s3-us-west-2.amazonaws.com/opswat-gears-cloud-clients/linux_installer/latest/opswatclient_deb.tar
@@ -220,6 +206,7 @@ sudo rm -rf opswatclient_deb
 sudo rm opswatclient_deb.tar
 echo "--->>> Installiation of OPSWAT Client is complite"
 
+###############################################################################
 
 echo "--->>> Install Ansible"
 sudo apt install software-properties-common
